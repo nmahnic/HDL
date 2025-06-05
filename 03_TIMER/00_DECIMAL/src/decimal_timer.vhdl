a5d6d38df008638fc501e6e6601c10d2fe47dd9d
Nico Mahnic <nico.mahnic94@gmail.com>
2025-06-05 19:58:29 +0200

    add decimal_timer

---
+++ 03_TIMER/00_DECIMAL/src/decimal_timer.vhdl
@@ -0,0 +1,72 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity decimal_timer is
+    port(
+        clk: in std_logic;
+        rst: in std_logic;
+        S0: out std_logic_vector(3 downto 0);
+        S1: out std_logic_vector(3 downto 0);
+        S2: out std_logic_vector(3 downto 0)
+    );
+end decimal_timer;
+
+architecture behavior of decimal_timer is
+    signal Co: std_logic_vector(2 downto 0);
+    signal max_count, internal_rst: std_logic;
+
+    component counter10c
+        port(
+            clk: in std_logic;
+            rst: in std_logic;
+            S: out std_logic_vector(3 downto 0);
+            co: out std_logic
+        );
+    end component;
+
+    component counter60c
+        port(
+            clk: in std_logic;
+            rst: in std_logic;
+            S: out std_logic_vector(5 downto 0);
+            co: out std_logic
+        );
+    end component;
+
+begin
+    c0: counter10c
+        port map(
+            clk => clk,
+            rst => internal_rst,
+            S => S0,
+            co => Co(0)
+        );
+
+    c1: counter10c
+        port map(
+            clk => Co(0),
+            rst => internal_rst,
+            S => S1,
+            co => Co(1)
+        );
+
+    c2: counter10c
+        port map(
+            clk => max_count,
+            rst => rst,
+            S => S2,
+            co => Co(2)
+        );
+
+    c3: counter60c
+        port map(
+            clk => clk,
+            rst => rst,
+            S => open,
+            co => max_count
+        );
+
+    internal_rst <= rst or max_count;
+
+end behavior;
---
+++ 03_TIMER/00_DECIMAL/tb/decimal_timer_tb.vhdl
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity decimal_timer_tb is
+end entity;
+
+architecture test of decimal_timer_tb is
+    signal clk, rst : std_logic;
+    signal S0, S1: std_logic_vector(3 downto 0);
+    signal S2: std_logic_vector(3 downto 0);
+begin
+    uut: entity work.decimal_timer
+        port map(
+            clk => clk,
+            rst => rst,
+            S0 => S0,
+            S1 => S1,
+            S2 => S2
+        );
+
+    process
+    begin
+        rst <= '0'; wait for 0.5 ps;
+        clk <= '1';  wait for 0.5 ps;
+        clk <= '0'; rst <= '1'; wait for 0.5 ps;
+        clk <= '1';  wait for 0.5 ps;
+        clk <= '0'; rst <= '0'; wait for 0.5 ps;
+        for i in 0 to 350 loop
+            clk <= '1';  wait for 0.5 ps;
+            clk <= '0';  wait for 0.5 ps;
+        end loop;
+        clk <= '0'; rst <= '1'; wait for 0.5 ps;
+        clk <= '1';  wait for 0.5 ps;
+        clk <= '0';  wait for 0.5 ps;
+        clk <= '1';  wait for 0.5 ps;
+        wait;
+    end process;
+end architecture;
