48aec8ec889e70267dd9965c33ff319f1c714c91
Nico Mahnic <nico.mahnic94@gmail.com>
2025-06-05 19:23:18 +0200

    add from 0 to 9 counter with carry_out

--- 02_COUNTERS/02_COUNTER_10_C/src/counter_10_c.vhdl
+++ 02_COUNTERS/02_COUNTER_10_C/src/counter_10_c.vhdl
@@ -6,7 +6,8 @@ entity counter10c is
     port(
         clk: in std_logic;
         rst: in std_logic;
-        S: out std_logic_vector(3 downto 0)
+        S: out std_logic_vector(3 downto 0);
+        co: out std_logic
     );
 end counter10c;
 
@@ -14,8 +15,27 @@ architecture behavior of counter10c is
     signal count: unsigned(3 downto 0);
     signal max_count: std_logic;
     signal internal_rst: std_logic;
+    signal internal_co: std_logic;
 
+    component ff_d
+        port(
+            clk: in std_logic;
+            rst: in std_logic;
+            d: in std_logic;
+            q: out std_logic;
+            n_q: out std_logic
+        );
+    end component;
 begin
+    ff_0: ff_d
+        port map(
+            clk => clk,
+            d => internal_co,
+            rst => rst,
+            q => co,
+            n_q => open
+        );
+
     process(clk, internal_rst)
     begin
         if(internal_rst = '1') then count <= (others => '0');
@@ -26,4 +46,6 @@ begin
     S <= std_logic_vector(count);
     max_count <= '1' when count = "1010" else '0';
     internal_rst <= rst or max_count;
+    internal_co <= '1' when count = "1001" else '0';
+    
 end behavior;
--- 02_COUNTERS/02_COUNTER_10_C/tb/counter_10_c_tb.vhdl
+++ 02_COUNTERS/02_COUNTER_10_C/tb/counter_10_c_tb.vhdl
@@ -5,14 +5,15 @@ entity counter_10_c_tb is
 end entity;
 
 architecture test of counter_10_c_tb is
-    signal clk, rst : std_logic;
+    signal clk, rst, co : std_logic;
     signal S: std_logic_vector(3 downto 0);
 begin
     uut: entity work.counter10c
         port map(
             clk => clk,
             rst => rst,
-            S => S
+            S => S,
+            co => co
         );
 
     process
@@ -24,6 +25,42 @@ begin
         clk <= '0'; rst <= '0'; wait for 0.5 fs;
         clk <= '1';  wait for 0.5 fs;
         clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+        clk <= '0';  wait for 0.5 fs;
+        clk <= '1';  wait for 0.5 fs;
+                clk <= '0';  wait for 0.5 fs;
         clk <= '1';  wait for 0.5 fs;
         clk <= '0';  wait for 0.5 fs;
         clk <= '1';  wait for 0.5 fs;
